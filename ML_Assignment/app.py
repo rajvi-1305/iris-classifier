dc0f73aeeec1f443bb70486e60b8aa52314592f1
RAJVI MANGROLIYA <167464724+rajvi-1305@users.noreply.github.com>
2025-04-14 16:13:52 +0530

    Initial Commit

---
+++ ML_Assignment/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# Load the trained model
+model = pickle.load(open("iris_model.pkl", "rb"))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        features = [
+            float(request.form['sepal_length']),
+            float(request.form['sepal_width']),
+            float(request.form['petal_length']),
+            float(request.form['petal_width'])
+        ]
+        prediction = model.predict([features])
+        class_names = ['Setosa', 'Versicolor', 'Virginica']
+        predicted_class = class_names[prediction[0]]
+        return render_template("index.html", prediction=f"Predicted Iris Class: {predicted_class}")
+    except Exception as e:
+        return render_template("index.html", prediction=f"Error: {e}")
+
+if __name__ == "__main__":
+    app.run(debug=True)
